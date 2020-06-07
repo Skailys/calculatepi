66b98dd848e08e628046f444c98fa97c95187309
fredie04 <fredie1104.kontakt@gmail.com>
2020-06-07 16:26:27 +0200

    Add a support for more methods

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use chrono::prelude::*;
 use std::time::SystemTime;
+use crate::CalculationMethode::{GregoryLeibniz, Nilkantha};
 
 fn get_pi() -> &'static str {
     return "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
@@ -20,31 +21,18 @@ fn how_close_is_the_result(result: String) -> usize {
     loop {
         if result_as_vec[counter + 1].eq(&pi_as_vec[counter + 1]) {
             counter = counter + 1;
-        } else {return counter}
+        } else { return counter }
     }
 }
 
-//TODO Add two difference modes
-
-fn main() {
-
-    //Get the start time
-    let start_time: DateTime<Local> = Local::now();
-    println!("The system time is {}!", start_time.time().to_string());
-
-    //Get start time from system
-    let start_time_of_os = SystemTime::now();
-
-    //Delicate vars
-    let debug_mode = false;
+fn gregory_leibniz(options: Options) -> f64 {
     let mut pi = 0.0;
+    let mut i_as_f64: f64;
+    let trails_as_f64 = (options.trails * 2) as f64;
     let mut next_plus = true;
-    let mut i_as_f64;
-    let number_of_trails: usize = 10 * 2;
-    let trails_as_f64 = number_of_trails as f64;
 
     //Numbers of calculating
-    for i in 1..number_of_trails {
+    for i in 1..(options.trails * 2) {
 
         //Get i as f64
         i_as_f64 = i as f64;
@@ -66,7 +54,7 @@ fn main() {
 
         //Change the boolean
         next_plus = !next_plus;
-        if debug_mode {
+        if options.debug_mode {
             println!("[Debug] next_plus is {}", next_plus);
         }
 
@@ -81,9 +69,55 @@ fn main() {
         println!(" ");
     }
 
+    return pi;
+}
+
+fn nilkantha(options: Options) -> usize {
+    //TODO Add the calculation from Nilkantha
+}
+
+enum CalculationMethode {
+    GregoryLeibniz,
+    Nilkantha,
+}
+
+struct Options {
+    debug_mode: bool,
+    trails: usize,
+    calculation_method: CalculationMethode,
+}
+
+fn main() {
+
+    //Get the start time
+    let start_time: DateTime<Local> = Local::now();
+    println!("The system time is {}!", start_time.time().to_string());
+
+    //Get start time from system
+    let start_time_of_os = SystemTime::now();
+
+    //Delicate options
+    let options = Options {
+        debug_mode: false,
+        trails: 10000,
+        calculation_method: CalculationMethode::GregoryLeibniz,
+    };
+
+    let mut pi = 0.0;
+    match options.calculation_method {
+        GregoryLeibniz => pi = gregory_leibniz(options),
+        Nilkantha => eprintln!("[Error] The method doesn't exist!"),
+    }
+
+
     println!("[Result] Finished! 𝜋 ≈ {}", pi);
     println!("[Info] Pi was calculated to {} decimal digits exactly", how_close_is_the_result(format!("{}", pi)) - 1);
 
     //Extension
-    println!("[Info] The calculating take {} secs!", start_time_of_os.elapsed().expect("[Debug] The time doesn't exist :O").as_secs());
+    let duration = start_time_of_os.elapsed().expect("[Debug] The time doesn't exist!");
+    if duration.as_secs() != 0 {
+        println!("[Info] The calculating take {} secs!", duration.as_secs())
+    } else {
+        println!("[Info] The calculating take {} secs!", format!("{:.2}", duration.as_secs_f32()));
+    }
 }
